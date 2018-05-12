12a95c419c80c0a980e315d60b90814f28c515a4
Mike Dettmer <mike@mikedettmer.com>
2018-05-11 20:47:49 -0400

    finishes lecture 24

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,88 @@
 'use strict';
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var appRoot = document.getElementById('app');
 
-var Person = function Person() {
-    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anon';
-
-    _classCallCheck(this, Person);
-
-    this.name = name;
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: []
 };
 
-var me = new Person('Mike D');
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+    var optionEl = e.target.elements.option;
+    if (optionEl.value) {
+        app.options.push(optionEl.value);
+        optionEl.value = '';
+        render();
+    }
+};
 
-console.log('me', me);
+var removeOptions = function removeOptions() {
+    app.options = [];
+    render();
+};
 
-var other = new Person();
-console.log('other', other);
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNum];
+    if (option) {
+        alert(option);
+    }
+};
+var render = function render() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h4',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'small',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'button',
+            { disabled: !app.options.length, onClick: onMakeDecision },
+            'What Should I Do?'
+        ),
+        React.createElement(
+            'button',
+            { onClick: removeOptions },
+            'Remove All'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            app.options.map(function (opt, index) {
+                return React.createElement(
+                    'li',
+                    { key: index },
+                    opt
+                );
+            })
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
+        )
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+render();
--- src/playground/classes.js
+++ src/playground/classes.js
@@ -1,12 +1,63 @@
 class Person {
-    constructor(name = 'Anon') {
+    constructor(name = 'Anon', age = 0) {
         this.name = name;
+        this.age = age;
+    }
+
+    getGreeting() {
+        return `Hi. I am ${this.name}!`;
+    }
+
+    getAge() {
+        return `I'm ${this.age} years old.`;
+    }
+
+    getDescription() {
+        return `${this.name} is ${this.age} year(s) old`;
     }
 }
 
-const me = new Person('Mike D');
+const me = new Student('Mike D', 30, 'fuck off');
 
-console.log('me', me);
-
-const other = new Person();
+console.log(me.getGreeting());
+console.log(me.hasMajor());
+const other = new Student();
 console.log('other', other);
+
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += `. They are a ${this.major} major`;
+        }
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super();
+        this.homeLocation = homeLocation;
+    }
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+    getGreeting() {
+        let greet = super.getGreeting();
+        if (this.hasHomeLocation) {
+            greet += ` I'm visiting from ${homeLocation}.`;
+        }
+        return greet;
+    }
+}
+
+let what = new Traveler('mike', 30, 'detroit');
+console.log('what.getGreeting()', what.getGreeting());
